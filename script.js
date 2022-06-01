7a1dd0c75901235f8c00ea143ad638b3bc518cfd
Ryan Palumbo <rynpal@gmail.com>
2022-06-01 14:37:04 -0400

    remove replit document reformat html/js line breaks add red border in css

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const randomQuoteIndex = (obj) => {
   return Math.floor(Math.random() * Object.keys(obj).length);
 };
 
-const colors = ['#ffeeee', '#eef1ff', 'eefcff', 'fcffee', 'fffaee', '#ffeef8', '#fbeeff', '#eefff8', '#eeffee', '#fff5ee'];
+const colors = [
+  "#ffeeee",
+  "#eef1ff",
+  "eefcff",
+  "fcffee",
+  "fffaee",
+  "#ffeef8",
+  "#fbeeff",
+  "#eefff8",
+  "#eeffee",
+  "#fff5ee",
+];
 
 //random color index function
 const randomColorIndex = () => {
@@ -13,30 +24,34 @@ let colorIndex = null;
 
 //render quote and color to DOM
 const render = () => {
-//change background color
+  //change background color
   let prevColorIndex = colorIndex;
   colorIndex = randomColorIndex();
 
   if (prevColorIndex == colorIndex) {
     colorIndex = randomColorIndex();
   }
-  
-$('body').animate({ backgroundColor: colors[colorIndex] }, 'slow');
 
-//change quote
-  $('#quote-container').fadeOut('slow',function () {
+  $("body").animate({ backgroundColor: colors[colorIndex] }, "slow");
 
-    $.get("https://type.fit/api/quotes",function (data) {
-      let index = data[randomQuoteIndex(data)]
-      $('#text').text(index.text);
-      if(index.author == null){
-      $('#author').text('Unknown')
-      } else {
-      $('#author').text(index.author);
-      }
-    },"JSON")
-    
-  }).fadeIn('slow');
+  //change quote
+  $("#quote-container")
+    .fadeOut("slow", function () {
+      $.get(
+        "https://type.fit/api/quotes",
+        function (data) {
+          let index = data[randomQuoteIndex(data)];
+          $("#text").text(index.text);
+          if (index.author == null) {
+            $("#author").text("Unknown");
+          } else {
+            $("#author").text(index.author);
+          }
+        },
+        "JSON"
+      );
+    })
+    .fadeIn("slow");
 };
 
 //render on document open
@@ -46,16 +61,20 @@ $(document).ready(function () {
 
 //element styling/functionality
 
-$('#quote-container').addClass('d-flex flex-column justify-content-center align-items-center text-center');
-$('#text').css('padding', '10px').addClass('fst-italic fs-2');
-$('#author').css('padding', '5px').addClass('fs-4');
+$("#quote-container").addClass(
+  "d-flex flex-column justify-content-center align-items-center text-center"
+);
+$("#text").css("padding", "10px").addClass("fst-italic fs-2");
+$("#author").css("padding", "5px").addClass("fs-4");
 
-$('#button-container').addClass('d-flex flex-row justify-content-center align-items-center');
+$("#button-container").addClass(
+  "d-flex flex-row justify-content-center align-items-center"
+);
 
-$('button').addClass('btn btn-success text-uppercase').
-on('click', render);
+$("button").addClass("btn btn-success text-uppercase").on("click", render);
 
-$('a').addClass('btn btn-primary').
-css('margin', '10px').
-attr('href', 'https://twitter.com/intent/tweet', 'target', '_top');
-$('i').addClass('bi bi-twitter');
+$("a")
+  .addClass("btn btn-primary")
+  .css("margin", "10px")
+  .attr("href", "https://twitter.com/intent/tweet", "target", "_top");
+$("i").addClass("bi bi-twitter");
